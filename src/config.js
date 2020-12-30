d392d3e856e7e69f02b9e74913f47e9f6de1db04
michael.fallorina@gmail.com <michael.fallorina@gmail.com>
2020-12-30 15:56:55 -0800

    deploying database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://mike@localhost/shootcast',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://mike@localhost/shootcast',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://mike@localhost/shootcast-test'
 }
